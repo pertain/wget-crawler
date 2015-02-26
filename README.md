b6650c7d9f6f41979fe2fdf03fa529a64b8c999d
william <4yellowpeppers@gmail.com>
2015-02-26 16:23:43 -0500

    submitted version

---
+++ README.md
@@ -0,0 +1,4 @@
+#wget crawler
+
+This is a pretty basic wget crawler. I wrote it for
+an assignment in my Information Retrieval course (Fall 2014).
---
+++ wget_crawler.sh
@@ -0,0 +1,12 @@
+# wget_crawler.sh
+#
+# This script is used to collect the first 100 links that wget finds
+# with a seed url of: computerscience.pages.tcnj.edu
+
+
+# This part grabs all pdf/html files
+wget -nv -r -o urlLog.txt -l 0 -t 1 --spider -w5 -A=*.{pdf,html} -e robots=on "computerscience.pages.tcnj.edu"
+
+
+# This part extracts the links from the log file & populates a txt file with them
+grep -Po '"\K[^"\047]+(?=["\047])' urlLog.txt | head -n100 | awk -F: '{print "http://"$1}' > urls.txt
