fa4896f578ae450f4b7b54679f4c1767057702ca
william <4yellowpeppers@gmail.com>
2015-03-28 22:15:16 -0400

    formatting cleanup

--- wget_crawler.sh
+++ wget_crawler.sh
@@ -5,8 +5,12 @@
 
 
 # This part grabs all pdf/html files
-wget -nv -r -o urlLog.txt -l 0 -t 1 --spider -w5 -A=*.{pdf,html} -e robots=on "computerscience.pages.tcnj.edu"
+wget -nv -r -o urlLog.txt -l 0 -t 1 \
+    --spider -w5 -A=*.{pdf,html} -e \
+    robots=on "computerscience.pages.tcnj.edu"
 
 
 # This part extracts the links from the log file & populates a txt file with them
-grep -Po '"\K[^"\047]+(?=["\047])' urlLog.txt | head -n100 | awk -F: '{print "http://"$1}' > urls.txt
+grep -Po '"\K[^"\047]+(?=["\047])' urlLog.txt | \
+    head -n100 | \
+    awk -F: '{print "http://"$1}' > urls.txt
